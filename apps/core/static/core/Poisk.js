af878decb5f2004d617f2e17b6432aeb263b4ec9
Daria <bezlikaya1@gmail.com>
2021-05-17 20:57:42 +0900

    Меню и поиск

--- apps/core/static/core/Poisk.js
+++ apps/core/static/core/Poisk.js
@@ -1,5 +1,3 @@
-
-
 $(function () {
     $('.FIO_Poisk').jqxCheckBox({width: 'auto'});
     $('.NomerKarty_Poisk').jqxCheckBox({width: 'auto'});
@@ -7,27 +5,16 @@ $(function () {
     $('.JKOtkrKarty_Poisk').jqxCheckBox({width: 'auto'});
     $('.OMS_Poisk').jqxCheckBox({width: 'auto'});
     $('.DataRojd_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.DataPostNaUchet_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.MestPostProj_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.SrokTekBer_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.SrokPoPoslAnk_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.JKVedBer_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.FIOUchVr_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.IshodBer_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.MestoIshoda_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.StRiskaPosleIsh_Poisk').jqxCheckBox({width: 'auto'});
-
     $('.StRiskaBer_Poisk').jqxCheckBox({width: 'auto'});
-
     $(".OsnovBtn_Poisk").jqxToggleButton({toggled: false});
     $(".OsnovBtn_Poisk").on('click', function () {
         var toggled = $(".OsnovBtn_Poisk").jqxToggleButton('toggled');
