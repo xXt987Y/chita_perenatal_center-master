2910eae5acbc502edf0a87c7920a1df8887a77cb
Daria <bezlikaya1@gmail.com>
2021-06-06 01:33:45 +0900

    toggle без эффекта зажатой кнопки

--- apps/core/static/core/Poisk.js
+++ apps/core/static/core/Poisk.js
@@ -1,30 +1,20 @@
-$(function () {
-    $('.FIO_Poisk').jqxCheckBox({width: 'auto'});
-    $('.NomerKarty_Poisk').jqxCheckBox({width: 'auto'});
-    $('.GodOtkrKarty_Poisk').jqxCheckBox({width: 'auto'});
-    $('.JKOtkrKarty_Poisk').jqxCheckBox({width: 'auto'});
-    $('.OMS_Poisk').jqxCheckBox({width: 'auto'});
-    $('.DataRojd_Poisk').jqxCheckBox({width: 'auto'});
-    $('.DataPostNaUchet_Poisk').jqxCheckBox({width: 'auto'});
-    $('.MestPostProj_Poisk').jqxCheckBox({width: 'auto'});
-    $('.SrokTekBer_Poisk').jqxCheckBox({width: 'auto'});
-    $('.SrokPoPoslAnk_Poisk').jqxCheckBox({width: 'auto'});
-    $('.JKVedBer_Poisk').jqxCheckBox({width: 'auto'});
-    $('.FIOUchVr_Poisk').jqxCheckBox({width: 'auto'});
-    $('.IshodBer_Poisk').jqxCheckBox({width: 'auto'});
-    $('.MestoIshoda_Poisk').jqxCheckBox({width: 'auto'});
-    $('.StRiskaPosleIsh_Poisk').jqxCheckBox({width: 'auto'});
-    $('.StRiskaBer_Poisk').jqxCheckBox({width: 'auto'});
-    $(".OsnovBtn_Poisk").jqxToggleButton({toggled: false});
-    $(".OsnovBtn_Poisk").on('click', function () {
-        var toggled = $(".OsnovBtn_Poisk").jqxToggleButton('toggled');
-    });
-    $(".PokazBtn_Poisk").jqxToggleButton({toggled: false});
-    $(".PokazBtn_Poisk").on('click', function () {
-        var toggled = $(".PokazBtn_Poisk").jqxToggleButton('toggled');
-    });
-    $(".UbratBtn_Poisk").jqxToggleButton({toggled: false});
-    $(".UbratBtn_Poisk").on('click', function () {
-        var toggled = $(".UbratBtn_Poisk").jqxToggleButton('toggled');
-    });
-})
+function regPoisk(evt, cityName) {
+    // Declare all variables
+    var i, regpoiskcontent, btnregpoisk;
+
+    // Get all elements with class="tabcontent" and hide them
+    regpoiskcontent = document.getElementsByClassName("regpoiskcontent");
+    for (i = 0; i < regpoiskcontent.length; i++) {
+        regpoiskcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    btnregpoisk = document.getElementsByClassName("btnregpoisk");
+    for (i = 0; i < btnregpoisk.length; i++) {
+        btnregpoisk[i].className = btnregpoisk[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
--- apps/core/static/core/TabMenu.js
+++ apps/core/static/core/TabMenu.js
@@ -7,13 +7,11 @@ function openMenu(evt, cityName) {
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
